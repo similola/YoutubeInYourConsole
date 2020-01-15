cd090b6e5cda542b52fd098f0559819ec67e502f
siawokoy <similola-a@hotmail.com>
2020-01-15 00:46:21 +0000

    Commit Helper Class

---
+++ src/main/java/com/youtubeinconsole/clientutils/YouTubeSearchUtil.java
@@ -0,0 +1,33 @@
+package com.youtubeinconsole.clientutils;
+
+import com.microsoft.azure.cognitiveservices.search.videosearch.implementation.BingVideoSearchAPIImpl;
+import com.microsoft.rest.credentials.ServiceClientCredentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class YouTubeSearchUtil {
+    public static BingVideoSearchAPIImpl getClient(final String subscriptionKey) {
+        return new BingVideoSearchAPIImpl ("https://api.cognitive.microsoft.com/bing/v7.0/",
+                new ServiceClientCredentials() {
+                    @Override
+                    public void applyCredentialsFilter(OkHttpClient.Builder builder) {
+                        builder.addNetworkInterceptor(
+                                new Interceptor() {
+                                    @Override
+                                    public Response intercept(Chain chain) throws IOException {
+                                            Request request = null;
+                                            Request original = chain.request();
+                                            Request.Builder requestBuilder = original.newBuilder()
+                                                    .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+                                            request = requestBuilder.build();
+                                            return chain.proceed(request);
+                                    }
+                                });
+                }
+                    });
+}
+}
