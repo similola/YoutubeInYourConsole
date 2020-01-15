6abf122b27359836c1d568ad6d99b0d5fe3cc392
siawokoy <similola-a@hotmail.com>
2020-01-15 01:07:49 +0000

    Remove key and searchterm from code and pass as argument to main class

--- src/main/java/com/youtubeinconsole/client/YoutubeSearch.java
+++ src/main/java/com/youtubeinconsole/client/YoutubeSearch.java
@@ -1,38 +1,43 @@
 package com.youtubeinconsole.client;
 
 import com.google.gson.Gson;
-import com.microsoft.azure.cognitiveservices.search.videosearch.BingVideos;
 import com.microsoft.azure.cognitiveservices.search.videosearch.implementation.BingVideoSearchAPIImpl;
-import com.microsoft.azure.cognitiveservices.search.videosearch.models.VideoObject;
 import com.microsoft.azure.cognitiveservices.search.videosearch.models.VideosModel;
 import com.youtubeinconsole.clientutils.YouTubeSearchUtil;
 
 public class YoutubeSearch {
 
-    public static void SearchYoutube(String subscriptionKey, String searchTerm){
+    public static void SearchYoutube(String subscriptionKey, String searchTerm) {
         BingVideoSearchAPIImpl client = YouTubeSearchUtil.getClient(subscriptionKey);
-        VideosModel videoResults = client.bingVideos().search(searchTerm,null);
+        VideosModel videoResults = client.bingVideos().search(searchTerm, null);
 
-        if (videoResults == null){
+        if (videoResults == null) {
             System.out.println("Didn't see any video result data..");
-        }
-        else{
-            if (videoResults.value().size() > 0){
+        } else {
+            if (videoResults.value().size() > 0) {
 
-                Gson gson=new Gson();
-                System.out.println(String.format("JSON: %s: ",gson.toJson(videoResults)));
+                Gson gson = new Gson();
+                System.out.println(String.format("JSON: %s: ", gson.toJson(videoResults)));
 
-            }
-            else{
+            } else {
                 System.out.println("Couldn't find video results!");
             }
         }
     }
 
     public static void main(String[] args) {
-        //main method to run class goes here, takes in search term as argument
-        //call bing search api
+        String key = "";
+        String searchTerm = "";
+        if (args.length > 0 && args.length == 2) {
+            //need to do better error handling later
+            System.out.println("Extracting subscription key and search term");
+            key = args[0];
+            searchTerm = args[1];
+        } else {
+            System.out.println("No command line arguments found. Please run with subscription key and search term as parameter");
+            System.out.println("Run like this on Command Line: java YoutubeSearch <subscriptionkey> <searchterm> ");
+        }
 
-        YoutubeSearch.SearchYoutube("subscriptionkeygoeshere","Beyonce");
+        YoutubeSearch.SearchYoutube(key, searchTerm);
     }
 }
